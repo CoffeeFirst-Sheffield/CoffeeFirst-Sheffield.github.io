a0fd3649f3ff0ae0c9bc55bc1f2f11662e2d0b19
AishaSKhan <askhan2@sheffield.ac.uk>
2016-11-19 23:43:00 +0000

    Social media buttons and adjusting js
    Added in social media buttons and tried adjusting the js script to make
    nav bar elements expand when you hove over them (didn't work yet, but
    left code in as may be along the right lines).

--- demo.js
+++ demo.js
@@ -1,3 +1,16 @@
 $(function(){
 	$('h1').css('color','#e68a00');
 })
+
+<!--Trying to make nav bar text expand a bit when you hover over them-->
+$("li").hover(function(){
+	$(this).expand(100);
+});
+
+$( "h1" )
+  .mouseover(function() {
+    $( this ).fadeOut(100);
+  })
+  .mouseout(function() {
+    $( this ).fadeIn(500);
+  });
